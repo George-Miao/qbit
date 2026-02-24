e420565a3fe0d712ff4719e18dcf56e65b9d44ee
George Miao <gm@miao.dev>
2026-02-24 00:25:26 -0500

    chore: update flake

--- flake.nix
+++ flake.nix
@@ -6,66 +6,78 @@
     rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
-  outputs = {
-    rust-overlay,
-    nixpkgs,
-    ...
-  }: let
-    forAllSystems = function:
-      nixpkgs.lib.genAttrs [
-        "x86_64-linux"
-        "x86_64-darwin"
-        "aarch64-linux"
-        "aarch64-darwin"
-      ] (system:
-        function (import nixpkgs {
-          inherit system;
-          overlays = [(import rust-overlay)];
-        }));
-  in {
-    devShells = forAllSystems (
-      pkgs:
-        with pkgs; let
+  outputs =
+    {
+      rust-overlay,
+      nixpkgs,
+      ...
+    }:
+    let
+      forAllSystems =
+        function:
+        nixpkgs.lib.genAttrs
+          [
+            "x86_64-linux"
+            "x86_64-darwin"
+            "aarch64-linux"
+            "aarch64-darwin"
+          ]
+          (
+            system:
+            function (
+              import nixpkgs {
+                inherit system;
+                overlays = [ (import rust-overlay) ];
+              }
+            )
+          );
+    in
+    {
+      devShells = forAllSystems (
+        pkgs:
+        with pkgs;
+        let
           llvm = pkgs.llvmPackages_latest;
-          packages =
-            [
-              llvm.bintools
-              llvm.libstdcxxClang
-              openssl
-              pkg-config
-            ]
-            ++ (
-              if pkgs.system == "aarch64-darwin" || pkgs.system == "x86_64-darwin"
-              then [
+          packages = [
+            llvm.bintools
+            llvm.libstdcxxClang
+            openssl
+            pkg-config
+          ]
+          ++ (
+            if
+              pkgs.stdenv.hostPlatform.system == "aarch64-darwin"
+              || pkgs.stdenv.hostPlatform.system == "x86_64-darwin"
+            then
+              [
                 darwin.apple_sdk.frameworks.SystemConfiguration
               ]
-              else []
-            );
-        in rec {
+            else
+              [ ]
+          );
+        in
+        rec {
           default = nightly;
-          stable = mkShell.override {stdenv = stdenvNoLibs;} {
-            packages =
-              [
-                (rust-bin.stable.latest.default.override
-                  {
-                    extensions = ["rust-src"];
-                  })
-              ]
-              ++ packages;
+          stable = mkShell.override { stdenv = stdenvNoLibs; } {
+            packages = [
+              (rust-bin.stable.latest.default.override {
+                extensions = [ "rust-src" ];
+              })
+            ]
+            ++ packages;
           };
-          nightly = mkShell.override {stdenv = stdenvNoLibs;} {
-            packages =
-              [
-                (
-                  rust-bin.selectLatestNightlyWith (toolchain:
-                    toolchain.default.override {
-                      extensions = ["rust-src"];
-                    })
-                )
-              ]
-              ++ packages;
+          nightly = mkShell.override { stdenv = stdenvNoLibs; } {
+            packages = [
+              (rust-bin.selectLatestNightlyWith (
+                toolchain:
+                toolchain.default.override {
+                  extensions = [ "rust-src" ];
+                }
+              ))
+            ]
+            ++ packages;
           };
         }
-    );
-  };
+      );
+    };
 }
